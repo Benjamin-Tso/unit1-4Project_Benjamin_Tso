a4361957cf46c883dc4b6ffc85d0d6b579382b99
Benjamin Tso <benjamint68@nycstudents.net>
2023-10-31 14:09:34 -0400

    started object class, design document drafted

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,3 +1,59 @@
+import java.util.*;
 public class Polynomial {
-    private
+    private String expression;
+    private ArrayList<String> terms;
+    private ArrayList<Double> coefficients;
+    private ArrayList<Integer> exponents;
+    public Polynomial(String e)
+    {
+        expression = e.replaceAll(" ","");
+        terms = parseTerms();
+        coefficients = parseCoefficients();
+        exponents = parseExponents();
+    }
+    private ArrayList<String> parseTerms()
+    {
+        ArrayList<String> t = new ArrayList<String>();
+        for (int i = 0; i<expression.length(); i++)
+        {
+
+        }
+        return t;
+    }
+    private ArrayList<Double> parseCoefficients()
+    {
+        ArrayList<Double> c = new ArrayList<Double>();
+        for(String term: terms)
+        {
+
+        }
+        return c;
+    }
+    private ArrayList<Integer> parseExponents()
+    {
+       ArrayList<Integer> e = new ArrayList<Integer>();
+       for(String term: terms)
+       {
+
+       }
+       return e;
+    }
+    public String solveExpressionA(int x)
+    {
+        int out = 0;
+        for(int i = 0; i<coefficients.size();i++);
+        {
+
+        }
+        return String.valueOf(out);
+    }
+    public String solveExpressionB(int start, int end)
+    {
+        String out = "";
+        for (int i = start; i<=end; i+=(end-start)/10)
+        {
+
+        }
+        return out;
+    }
 }
--- src/PolynomialRunner.java
+++ src/PolynomialRunner.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class PolynomialRunner {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        System.out.println("welcome to the polynomial \"solver\", please enter a polynomial expression\nuse ^ to represent to the power of ");
     }
 }
